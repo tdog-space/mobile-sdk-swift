4dbf586df581a1f37d2aac1d77ef153dfb52d627
thanhtan541 <thanhtan004100@gmail.com>
2025-07-28 14:04:13 +0700

    feat: update

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Swift Package: MobileSdkRs
+
+import PackageDescription;
+
+let package = Package(
+    name: "MobileSdkRs",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "MobileSdkRs",
+            targets: ["MobileSdkRs"]
+        )
+    ],
+    dependencies: [ ],
+    targets: [
+        .binaryTarget(name: "RustFramework", path: "./RustFramework.xcframework"),
+        .target(
+            name: "MobileSdkRs",
+            dependencies: [
+                .target(name: "RustFramework")
+            ]
+        ),
+    ]
+)
